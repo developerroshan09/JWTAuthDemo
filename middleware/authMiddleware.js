c70ed0240fde641f00ce9418f31ec8271f2b3bd6
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2025-09-14 20:33:03 +0545

    logout implemented

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+
+const blacklist = require("../blacklist");
 const jwt = require('jsonwebtoken');
+
 function verifyToken(req, res, next) {
     const token = req.header('Authorization');
     if (!token) return res.status(401).json({ error: 'Access denied' });
+
+    // Check blacklist
+    if (blacklist.has(token)) {
+        return res.status(403).json({ error: "Token is invalid (loggedout)" });
+    }
     try {
-        const decoded = jwt.verify(token, 'your-secret-key');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.userId;
         next();
     } catch (error) {
